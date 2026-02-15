f3ece4639dee1a091b347f776a35230dbbde0f91
Renan Vinícius <renanviniciussds@gmail.com>
2026-02-15 01:21:25 -0400

    add: introduction to vector and string functions

---
+++ src/com/nuxyel/basics/Vector.java
@@ -0,0 +1,14 @@
+package com.nuxyel.basics;
+
+public class Vector {
+    static void main() {
+        String s  = "people like to walk around";
+
+        String[] vect = s.split(" ");
+        System.out.println(vect[0]);
+        System.out.println(vect[1]);
+        System.out.println(vect[2]);
+        System.out.println(vect[3]);
+        System.out.println(vect[4]);
+    }
+}
