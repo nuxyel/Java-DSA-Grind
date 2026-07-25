3c12ce48a53adae9408fc44e465039dff1e631e2
Renan Vinícius <renanviniciussds@gmail.com>
2026-07-24 22:03:22 -0400

    chore: preserve local work before workstation reset

---
+++ src/com/nuxyel/challenges/ninjas.java
@@ -0,0 +1,23 @@
+package com.nuxyel.challenges;
+
+import java.util.Scanner;
+
+public class ninjas {
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+        int NUMERO_MAXIMO = 15;
+        String[] ninjas = new String[NUMERO_MAXIMO];
+
+        int ninjasCadastrados = 0;
+        int opcoes = 0;
+
+
+        System.out.println("\n===== Menu Ninja =====");
+        System.out.println("1. Cadastrar Ninja");
+        System.out.println("2. Listar Ninjas");
+        System.out.println("3. Sair");
+        System.out.print("Escolha uma opção: ");
+
+
+    }
+}
